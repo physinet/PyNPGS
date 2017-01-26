53392588d3b05fef1b9a91c0f2816d1497b4df5f
Brian Schaefer <physinet@gmail.com>
2017-01-26 14:48:00 -0500

    Fixed regex bug
    Regex was not working properly. Some parts of alignment marks were not
    changed to solid, and were kept as dashed windows. The regex pattern was
    too lenient, and allowed any number of integers strung together by
    spaces to pass, causing inaccurate identification of layer headers.

--- designcad/make_align.py
+++ designcad/make_align.py
@@ -32,8 +32,8 @@ def make_align(dc2file, layers, locations, window_size=50):
         if layer > 15:
             raise Exception('Avoid using layer numbers > 15. I\'m not sure how DC2 files deal with layer numbers above 15.')
 
-    pattern_layer = re.compile("^\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*$") # this is six integers separated by spaces
-    pattern_shape = re.compile("^\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}.[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*[0-9]{1,10}\s*$")
+    pattern_layer = re.compile("^\s*[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s*$") # this is six integers separated by spaces
+    pattern_shape = re.compile("^\s*[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}.[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s[0-9]{1,10}\s*$")
 
     ## Backup copy of DC2 file
     shutil.copyfile(dc2file, dc2file.split('.')[0]+'_backup.dc2') # copy in place
@@ -48,12 +48,12 @@ def make_align(dc2file, layers, locations, window_size=50):
     ## Find shape headings and change to solid
     for i, line in enumerate(lines):
         for match in re.finditer(pattern_layer, line): # find the layer heading
-            layer_number = int(lines[i].split()[1]) # the layer number is the second number in the layer heading
+            layer_number = int(line.split()[1]) # the layer number is the second number in the layer heading
             # we then continue through the next few lines which correspond to the layer we just identified
         for match in re.finditer(pattern_shape, line):
             if layer_number in layers:
                 ## Change alignment marks to solid
-                split = lines[i].split()
+                split = line.split()
                 split[4] = str(0) #
                 lines[i] = ' '.join(split) + '\n'
 
